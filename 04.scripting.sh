ce9911573744cb4a5954d8415caa1e59a2a221d4
Palsonroy <Palsonroy123@gmail.com>
2024-05-29 15:35:51 +0530

    installing mysql

--- 04.scripting.sh
+++ 04.scripting.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 ID=$(id -u)
+
 if [ $ID -ne 0 ]
 then 
-echo "its a root user"
+    echo "its a root user"
 else
-echo "its  not a root user"
+    echo "its  not a root user"
 fi
+
+yum install mySQL -y
