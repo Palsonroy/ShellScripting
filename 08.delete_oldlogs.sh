dee358948e380f4bfbf5f4bd03335a07e1f32ba1
Palsonroy <Palsonroy123@gmail.com>
2024-06-03 15:12:46 +0530

    deletelogs

--- 08.delete_oldlogs.sh
+++ 08.delete_oldlogs.sh
@@ -2,15 +2,16 @@
 
 SOURCE_DIR="/tmp/shellscript-logs"
 
-R="e\[31m"
-G="e\[32m"
-Y="e\[33m"
-N="e\[0m"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 if [ ! -d SOURCE_DIR ]
 then
     echo -e "$R source_dir : $SOURCE_DIR doesnt exit.$N"
 fi
+
 FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
 
 while IFS= read -r line 
