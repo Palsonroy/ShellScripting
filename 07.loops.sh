bca39a9a96446916bfc7cf11149d4ca849484c67
Palsonroy <Palsonroy123@gmail.com>
2024-06-03 15:03:34 +0530

    deletelogs

--- 07.loops.sh
+++ 07.loops.sh
@@ -15,7 +15,7 @@ VALIDATE(){
 
     if [ $1 -ne 0 ]
     then 
-        echo -e "$2 ... $2 failed $N"
+        echo -e "$2 ... $2 failed $N" 
         exit 1
     else
         echo -e "$2 ...$G success $N"
