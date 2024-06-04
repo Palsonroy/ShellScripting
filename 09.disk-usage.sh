e837cce8e238574909002c5f8608a80b77477e2d
Palsonroy <Palsonroy123@gmail.com>
2024-06-04 14:48:09 +0530

    disk usage

---
+++ 09.disk-usage.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep -vE 'tmp|file')
+DISK_TRESHOLD=1
+message=""
+
+while IFS= read line
+do 
+    usage=$(echo $line | awk '{print $6f}' | cut -d % -f1)
+    Partition=$(echo $line | awk '{print $1f}')
+    if [ $usage -ge $DISK_TRESHOLD ]
+    then
+        message+="High disk usage on $Partition: $usage"
+    fi
+done <<< $DISK_USAGE
